eefab2095e347ca3da4c0e88dd25a95e4bc1b82a
iddimsangi <adammsangi@gmail.com>
2022-01-04 20:02:04 +0300

    footer layout fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import logo from "../images/logo.svg";
+import twitter from "../images/icon-twitter.svg";
+import facebook from "../images/icon-facebook.svg";
+import instagram from "../images/icon-instagram.svg";
+import youtube from "../images/icon-youtube.svg";
+import pinrest from "../images/icon-pinterest.svg";
 import phone from "../images/image-mockups.png";
 import "./App.scss";
 
@@ -45,7 +50,42 @@ function App() {
         </div>
       </header>
       </main>
-      <footer className="App--footer"></footer>
+      <footer className="App--footer">
+        <div className="App--footer--container1">
+        <img src={logo} alt="logo" className="footerLogo"/>
+        <ul className="App--footer--container1--socials">
+          <li>
+          <img src={facebook} alt="facebook-icon"/>
+          </li>
+          <li>
+          <img src={twitter} alt="twitter-icon"/>
+          </li>
+          <li>
+          <img src={youtube} alt="youtube-icon"/>
+          </li>
+          <li>
+          <img src={pinrest} alt="pinrest-icon"/>
+          </li>
+          <li>
+          <img src={pinrest} alt="pinrest-icon"/>
+          </li>
+        </ul>
+        </div>
+        <ul className="App--footer--container1--container2">
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+        <ul className="App--footer--container3">
+          <li><a href="#">Carriers</a></li>
+          <li><a href="#">Suports</a></li>
+          <li><a href="#">Private policy</a></li>
+        </ul>
+        <div>
+          <a href="#" className="btn">Request Invite</a>
+          <p>@EasyBank.All rights Reserved</p>
+        </div>
+      </footer>
     </div>
   );
 }
